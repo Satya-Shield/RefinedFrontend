addbc73b51c84a8cf48a28c018606d722c122c89
kunj_10 <kunjvgoyal30@gmail.com>
2025-11-13 23:28:16 +0530

    Proxy Fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
+  reactStrictMode: true,
+
+  async rewrites() {
     return [
       {
         source: '/api_bk/:path*',
         destination: 'http://34.93.122.16:8000/api/:path*',
       },
-    ]
+    ];
+  },
+
+  trailingSlash: false,
+
+  experimental: {
+    runtime: 'nodejs',
   },
 };
 
